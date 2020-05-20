60b67b5452033ae810659dc8c0340cd76c6e07a9
LuciferFromDoomsday <48617655+LuciferFromDoomsday@users.noreply.github.com>
2020-05-20 16:32:56 +0600

    Initial Commit

---
+++ News app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  News app
+//
+//  Created by admin on 5/20/20.
+//  Copyright © 2020 Naimankhan Ayan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
